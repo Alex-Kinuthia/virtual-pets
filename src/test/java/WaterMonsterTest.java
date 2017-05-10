fe712fd610823e5c8361ca14fc13a8820b92ed0c
Alex-Kinuthia <alexkinuthia125@gmail.com>
2017-05-10 11:40:02 +0300

    modified WaterMonsterTest.java

--- src/test/java/WaterMonsterTest.java
+++ src/test/java/WaterMonsterTest.java
@@ -289,6 +289,12 @@ public class WaterMonsterTest {
     assertTrue(testWaterMonster.getWaterLevel() > (WaterMonster.MAX_WATER_LEVEL / 2));
   }
 
-
+  @Test(expected = UnsupportedOperationException.class)
+  public void kindling_throwsExceptionIfWaterLevelIsAtMaxValue(){
+    WaterMonster testWaterMonster = new WaterMonster("Drippy", 1);
+    for(int i = WaterMonster.MIN_ALL_LEVELS; i <= (WaterMonster.MAX_WATER_LEVEL); i++){
+      testWaterMonster.water();
+    }
+  }
 
 }
